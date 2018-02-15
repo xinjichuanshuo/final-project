4b66f456c7b073fa04673a42a9d27fc7f3fb2b7d
JiaCong <18851280888@163.com>
2018-02-15 14:16:37 +0800

    添加轮播图

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 window.onload = function () {
+  var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      stopOnLastSlide: false,
+      disableOnInteraction: true,
+    },
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })    
+
   var arr = [];
   var eles = document.getElementsByClassName("chart");
   Array.prototype.forEach.call(eles, function (ele) {
@@ -161,6 +176,9 @@ window.onload = function () {
   };
 
   var educationOption = {
+    title: {
+      text: '用户教育程度分布'
+    },
     tooltip : {
         trigger: 'axis',
         axisPointer : {            // 坐标轴指示器，坐标轴触发有效
@@ -185,7 +203,7 @@ window.onload = function () {
         {
             type : 'category',
             axisTick : {show: false},
-            data : ['高中中专及以下','大专','本科','双学士','硕士','博士','博士后']
+            data : ['高中中专及以下','大专','本科','双学士','硕士','博士']
         }
     ],
     series : [
@@ -199,7 +217,7 @@ window.onload = function () {
                     position: 'right'
                 }
             },
-            data:[1424, 2390, 2441, 81, 288, 52, 18]
+            data:[1424, 2390, 2441, 81, 288, 70]
         },
         {
             name:'女性',
@@ -211,7 +229,7 @@ window.onload = function () {
                     position: 'right'
                 }
             },
-            data:[718, 1484, 1172, 32, 134, 5, 4]
+            data:[718, 1484, 1172, 32, 134, 7]
         }// },
         // {
         //     name:'支出',
@@ -228,211 +246,8 @@ window.onload = function () {
     ]
   };
 
-  var pieData = genData();
-  var pieOption = {
-    title : {
-        text: '单位性质分布',
-        x:'center'
-    },
-    tooltip : {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        type: 'scroll',
-        orient: 'vertical',
-        right: 10,
-        top: 20,
-        bottom: 20,
-        data: pieData.nameList,
-        selected: pieData.selected
-    },
-    series : [
-        {
-            name: '就业单位',
-            type: 'pie',
-            radius : '55%',
-            center: ['40%', '50%'],
-            data: pieData.seriesData,
-            itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
-        }
-    ]
-  };
-
-  var dataAxis = ['南京', '上海', '深圳', '苏州', '北京', '杭州', '无锡', '合肥', '广州', '常州'];
-  var data = [1280, 698, 298, 260, 240, 237, 94, 87, 83, 78];
-  var yMax = 1500;
-  var dataShadow = [];
-
-  for (var i = 0; i < data.length; i++) {
-      dataShadow.push(yMax);
-  }
-
-  var barOption = {
-    title: {
-        text: '热门城市人数统计',
-    },
-    xAxis: {
-        data: dataAxis,
-        axisLabel: {
-            inside: true,
-            textStyle: {
-                color: '#fff'
-            }
-        },
-        axisTick: {
-            show: false
-        },
-        axisLine: {
-            show: false
-        },
-        z: 10
-    },
-    yAxis: {
-        axisLine: {
-            show: false
-        },
-        axisTick: {
-            show: false
-        },
-        axisLabel: {
-            textStyle: {
-                color: '#999'
-            }
-        }
-    },
-    dataZoom: [
-        {
-            type: 'inside'
-        }
-    ],
-    series: [
-        { // For shadow
-            type: 'bar',
-            itemStyle: {
-                normal: {color: 'rgba(0,0,0,0.05)'}
-            },
-            barGap:'-100%',
-            barCategoryGap:'40%',
-            data: dataShadow,
-            animation: false
-        },
-        {
-            type: 'bar',
-            itemStyle: {
-                normal: {
-                    color: new echarts.graphic.LinearGradient(
-                        0, 0, 0, 1,
-                        [
-                            {offset: 0, color: '#83bff6'},
-                            {offset: 0.5, color: '#188df0'},
-                            {offset: 1, color: '#188df0'}
-                        ]
-                    )
-                },
-                emphasis: {
-                    color: new echarts.graphic.LinearGradient(
-                        0, 0, 0, 1,
-                        [
-                            {offset: 0, color: '#2378f7'},
-                            {offset: 0.7, color: '#2378f7'},
-                            {offset: 1, color: '#83bff6'}
-                        ]
-                    )
-                }
-            },
-            data: data
-        }
-    ]
-};
-
-  var scatterOption = {
-    xAxis: {},
-    yAxis: {},
-    series: [{
-        symbolSize: 20,
-        data: [
-            [19.28, 8.04],
-            [15.95, 6.95],
-            [14.59, 7.58],
-            [10.13, 8.81],
-            [8.44, 8.33],
-            [6.02, 9.96],
-            [5.80, 7.24],
-            [4.19, 4.26],
-            [2.48, 10.84],
-            [2.46, 4.82],
-            [2.07, 13.34],
-            [1.59, 1.23],
-            [1.43, 6.57],
-            [1.35, 10.48],
-            [1.09, 0.94]
-        ],
-        type: 'scatter'
-    }]
-  };
-
-  lineOption = {
-    xAxis: {
-        type: 'category',
-        data: ['2013', '2014', '2015', '2016', '2017']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [{
-        data: [41, 43, 57, 79, 143],
-        type: 'line'
-    }]
-};
-
   echarts.init(arr[0]).setOption(mapOption);
   echarts.init(arr[1]).setOption(ageOption);
   echarts.init(arr[2]).setOption(educationOption);
-  var barChart = echarts.init(arr[4]);
-  barChart.setOption(barOption);
-  echarts.init(arr[5]).setOption(scatterOption);
-  echarts.init(arr[6]).setOption(lineOption);
-
-  function genData() {
-    var nameList = [
-      '国有企业','民营企业','三资企业','高等教育单位','国家机关', '医疗卫生单位', '其他事业单位', '科研设计单位', '中初教育单位', '部队', '自主创业', '其他'
-    ];
-    var count = [
-      1222, 1204, 768, 512, 470, 257, 197, 134, 91, 46, 13, 122
-    ];
-    
-    var seriesData = [];
-    var selected = {};
-    for (var i = 0; i < 12; i++) {
-      seriesData.push({
-          name: nameList[i],
-          value: count[i]
-      });
-      selected[nameList[i]] = seriesData[i];
-    }
-
-    return {
-        nameList: nameList,
-        seriesData: seriesData,
-        selected: selected
-    };
-  };
-
-  // Enable data zoom when user click bar.
-  var zoomSize = 6;
-  barChart.on('click', function (params) {
-      barChart.dispatchAction({
-          type: 'dataZoom',
-          startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
-          endValue: dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
-      });
-  });
 
 };
